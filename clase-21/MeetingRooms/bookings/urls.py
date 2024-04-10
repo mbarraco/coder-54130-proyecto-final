5c906752caa5831edcda4c28c87dc8b2be56a682
Mariano Barraco <marianobarraco@gmail.com>
2024-04-09 21:18:36 -0300

    clase 21

---
+++ clase-21/MeetingRooms/bookings/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from .views import (
+    home_view,
+    detail_view,
+    list_view,
+    search_view,
+    search_with_form_view,
+    create_with_form_view,
+    create_sala_with_form_view,
+    detail_sala_view
+)
+
+urlpatterns = [
+    path("", home_view),
+    path("detail/<booking_id>", detail_view),
+    path("list/", list_view, name="bookings-list"),
+    path("buscar/<nombre_de_usuario>", search_view),
+    # Clase 21: formularios
+    path("buscar-con-formulario/", search_with_form_view, name="zzz"),
+    path("crear-reserva-con-formulario/", create_with_form_view, name="yyy"),
+    path("crear-sala-con-formulario/", create_sala_with_form_view, name="crear-sala-con-form"),
+    path("detail-sala/<sala_id>", detail_sala_view),
+]
